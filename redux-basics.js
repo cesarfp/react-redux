f3cb3d4ca7d8ff40b524ecc0d12f9cf972a0f4cf
César <cesar.franco@4thsource.com>
2019-04-01 17:39:24 -0600

    class 281

--- redux-basics.js
+++ redux-basics.js
@@ -29,11 +29,14 @@ const store = createStore(rootReducer)
 console.log(store.getState())
 
 
+//Subcription
+store.subscribe(()=>{
+	console.log('[Subscription]', store.getState())
+})
+
 //Dispatching Action
 store.dispatch({type:'INC_COUNTER'})
 store.dispatch({type:'ADD_COUNTER', value:10})
 console.log(store.getState())
 
 
-
-//Subcription
